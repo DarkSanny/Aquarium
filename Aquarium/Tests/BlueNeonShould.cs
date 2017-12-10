dec87aa8758f6aa091cc7642b6139831b5107841
Alexandr Rechkalov <streetonemg10@mail.ru>
2017-12-10 20:17:14 +0500

    подключил нужные библиотеки

---
+++ Aquarium/Tests/BlueNeonShould.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Aquarium.Tests
+{
+	[TestFixture]
+	public class BlueNeonShould
+	{
+		private IAquarium _aquarium;
+		private Point _startPosition;
+		private double _startDirection;
+		private BlueNeon _neon;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_aquarium = A.Fake<IAquarium>();
+			_startPosition = new Point(0, 0);
+			_startDirection = 0;
+			_neon = new BlueNeon(_aquarium, _startPosition, _startDirection);
+		}
+
+
+		[Test]
+		public void SimpleTestForExample()
+		{
+			_neon.GetCollisionType().Should().Be(ObjectType.BlueNeon);
+		}
+
+	}
+}
