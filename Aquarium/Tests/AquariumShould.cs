94b55d14e33741872ea622b21a9e324fe5525828
RubyRaven0812 <goodbad@twilightrussia.ru>
2017-12-15 17:26:28 +0500

    tests

--- Aquarium/Tests/AquariumShould.cs
+++ Aquarium/Tests/AquariumShould.cs
@@ -1,10 +1,27 @@
 ﻿using NUnit.Framework;
+using FakeItEasy;
+using FluentAssertions;
+using System.Drawing;
 
 namespace Aquarium.Tests
 {
 	[TestFixture]
 	public class AquariumShould
 	{
+        private IAquarium _aquarium;
+        private Size _size;
+        private GameObject _object;
+
+
+        [SetUp]
+        public void SetUp ()
+        {
+            _aquarium = A.Fake<IAquarium>();
+            _object = A.Fake<GameObject>();
+            _size = new Size(100, 100);
+
+        }
+
 
 	}
 }
