33b6710766970d9d31c34b6f521644d308a4fe9a
Alexandr Rechkalov <streetonemg10@mail.ru>
2017-12-15 20:18:30 +0500

    what

--- Aquarium/Fishes/BlueNeon.cs
+++ Aquarium/Fishes/BlueNeon.cs
@@ -3,48 +3,48 @@ using Aquarium.Brains;
 
 namespace Aquarium.Fishes
 {
-    public class BlueNeon : Fish
-    {
-        private readonly IAquarium _aquarium;
-        private Point _location;
-        public bool IsLeader { get; set; }
+	public class BlueNeon : Fish
+	{
+		private readonly IAquarium _aquarium;
+		private Point _location;
+		public bool IsLeader { get; set; }
 
-        public BlueNeon(IAquarium aquarium, Point location, double direction, Size size) : base(size)
-        {
-            _aquarium = aquarium;
-            _location = location;
-            Speed = 5;
-            Force = 0;
-            Direction = direction;
+		public BlueNeon(IAquarium aquarium, Point location, double direction, Size size) : base(size)
+		{
+			_aquarium = aquarium;
+			_location = location;
+			Speed = 5;
+			Force = 0;
+			Direction = direction;
 			SetBrain(new BlueNeonBrain(this, aquarium));
-        }
+		}
 
-        public override void Collision(ObjectType objectType, GameObject obj)
-        {
-	        if (objectType == GetCollisionType()) return;
-	        OnShouldDie();
-        }
+		public override void Collision(ObjectType objectType, GameObject obj)
+		{
+			if (objectType == GetCollisionType()) return;
+			OnShouldDie();
+		}
 
-        public override ObjectType GetCollisionType()
-        {
-            return ObjectType.BlueNeon;
-        }
+		public override ObjectType GetCollisionType()
+		{
+			return ObjectType.BlueNeon;
+		}
 
-        public override Point GetLocation()
-        {
-            return _location;
-        }
+		public override Point GetLocation()
+		{
+			return _location;
+		}
 
-        public override bool IsShouldCollise(ObjectType objectType)
-        {
+		public override bool IsShouldCollise(ObjectType objectType)
+		{
 			// todo: false если рыбу нельзя съесть
-            return objectType != GetCollisionType() || IsLeader;
-        }
+			return objectType != GetCollisionType() || IsLeader;
+		}
 
-        public override void Move()
-        {
+		public override void Move()
+		{
 			Brain.Think();
-	        _location = GetNextPoint(_aquarium);
-        }
-    }
+			_location = GetNextPoint(_aquarium);
+		}
+	}
 }
